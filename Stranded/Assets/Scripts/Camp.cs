6dab34e9c1d1e23602e337cf258341843b5c2398
Musicaljelly <musicaljelly@gmail.com>
2015-01-25 13:37:09 -0800

    Fixed Walt's animation, and a few other things.

--- Stranded/Assets/Scripts/Camp.cs
+++ Stranded/Assets/Scripts/Camp.cs
@@ -6,7 +6,7 @@ public class Camp : MonoBehaviour {
     static float shelterTime = 30f; // time in seconds
     static float shelterCounter = 0f;
     static float campfireCounter = 0f;
-    static float campfireTime = 3f; // Default = 15. time in seconds
+    static float campfireTime = 15f; // Default = 15. time in seconds
     static float campfireStartCounter = 0f;
     static float campfireStartTime = 20f; // time in seconds
     public static int campfireLv = 3;

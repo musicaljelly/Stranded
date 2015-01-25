47039a4a2f463562750b0a9599439e637a22a4cf
Musicaljelly <musicaljelly@gmail.com>
2015-01-24 19:53:15 -0800

    Radial menu now works with the controller. Fixed player movement with the controller.

--- Stranded/Assets/Scripts/Player.cs
+++ Stranded/Assets/Scripts/Player.cs
@@ -21,93 +21,69 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        // Gamepad controls
-        if (GamePad.GetAxis(GamePad.Axis.LeftStick, GamePad.Index.One) == new Vector2(0,1))
-        {
-            motion = new Vector2(motion.x, 1);
-        }
-        else if (GamePad.GetAxis(GamePad.Axis.LeftStick, GamePad.Index.One) == new Vector2(0, -1))
-        {
-            motion = new Vector2(motion.x, -1);
-        }
-        else
-        {
-            motion = new Vector2(motion.x, 0);
-        }
 
-        if (GamePad.GetAxis(GamePad.Axis.LeftStick, GamePad.Index.One) == new Vector2(-1, 0))
-        {
-            motion = new Vector2(-1, motion.y);
-        }
-        else if (GamePad.GetAxis(GamePad.Axis.LeftStick, GamePad.Index.One) == new Vector2(1, 0))
-        {
-            motion = new Vector2(1, motion.y);
-        }
-        else
-        {
-            motion = new Vector2(0, motion.y);
-        }
+		if (Input.GetAxis ("TriggersR_0") < 0.5) {
 
-        // Keyboard controls
-        if (Input.GetKey(KeyCode.W))
-        {
-            motion = new Vector2(motion.x, 1);
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            motion = new Vector2(motion.x, -1);
-        }
-        else
-        {
-            motion = new Vector2(motion.x, 0);
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            motion = new Vector2(-1, motion.y);
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            motion = new Vector2(1, motion.y);
-        }
-        else
-        {
-            motion = new Vector2(0, motion.y);
-        }
-
-        motion.Normalize();
-		Vector3 oldPos = transform.position;
-        transform.Translate((motion * speed) * Time.deltaTime);
-
-		foreach (GameObject obstacle in obstacles) {
-			Bounds obstacleBounds = obstacle.collider2D.bounds;
-			obstacleBounds.center = new Vector3(obstacleBounds.center.x, obstacleBounds.center.y, 0);
-			Bounds playerBounds = collider2D.bounds;
-			playerBounds.center = new Vector3(playerBounds.center.x, playerBounds.center.y, 0);
-			if (playerBounds.Intersects (obstacleBounds)) {
-				transform.position = oldPos;
-				break;
+			if (Input.GetKey (KeyCode.W) || GamePad.GetAxis(GamePad.Axis.LeftStick, GamePad.Index.One) == new Vector2(0,1))
+			{
+				motion = new Vector2(motion.x, 1);
 			}
-		}
+			else if (Input.GetKey (KeyCode.S) || GamePad.GetAxis(GamePad.Axis.LeftStick, GamePad.Index.One) == new Vector2(0, -1))
+			{
+				motion = new Vector2(motion.x, -1);
+			}
+			else
+			{
+				motion = new Vector2(motion.x, 0);
+			}
+			
+			if (Input.GetKey (KeyCode.A) || GamePad.GetAxis(GamePad.Axis.LeftStick, GamePad.Index.One) == new Vector2(-1, 0))
+			{
+				motion = new Vector2(-1, motion.y);
+			}
+			else if (Input.GetKey (KeyCode.D) || GamePad.GetAxis(GamePad.Axis.LeftStick, GamePad.Index.One) == new Vector2(1, 0))
+			{
+				motion = new Vector2(1, motion.y);
+			}
+			else
+			{
+				motion = new Vector2(0, motion.y);
+			}
+			
+			motion.Normalize();
+			Vector3 oldPos = transform.position;
+			transform.Translate((motion * speed) * Time.deltaTime);
 
-		// Update the camera. This needs to be here to eliminate stutter.
-		Vector3 oldCamPos = cam.transform.position;
-		Vector3 newPos = transform.position;
-		newPos.z = cam.transform.position.z;
-		cam.transform.position = newPos;
-		
-		Bounds backgroundBounds = background.collider2D.bounds;
-		Bounds cameraBounds = cam.collider2D.bounds;
-		
-		if (cameraBounds.min.x < backgroundBounds.min.x || cameraBounds.max.x > backgroundBounds.max.x) {
-			newPos.x = oldCamPos.x;
+			foreach (GameObject obstacle in obstacles) {
+				Bounds obstacleBounds = obstacle.collider2D.bounds;
+				obstacleBounds.center = new Vector3(obstacleBounds.center.x, obstacleBounds.center.y, 0);
+				Bounds playerBounds = collider2D.bounds;
+				playerBounds.center = new Vector3(playerBounds.center.x, playerBounds.center.y, 0);
+				if (playerBounds.Intersects (obstacleBounds)) {
+					transform.position = oldPos;
+					break;
+				}
+			}
+			
+			// Update the camera. This needs to be here to eliminate stutter.
+			Vector3 oldCamPos = cam.transform.position;
+			Vector3 newPos = transform.position;
+			newPos.z = cam.transform.position.z;
+			cam.transform.position = newPos;
+			
+			Bounds backgroundBounds = background.collider2D.bounds;
+			Bounds cameraBounds = cam.collider2D.bounds;
+			
+			if (cameraBounds.min.x < backgroundBounds.min.x || cameraBounds.max.x > backgroundBounds.max.x) {
+				newPos.x = oldCamPos.x;
+			}
+			
+			if (cameraBounds.min.y < backgroundBounds.min.y || cameraBounds.max.y > backgroundBounds.max.y) {
+				newPos.y = oldCamPos.y;
+			}
+			
+			cam.transform.position = newPos;
 		}
-		
-		if (cameraBounds.min.y < backgroundBounds.min.y || cameraBounds.max.y > backgroundBounds.max.y) {
-			newPos.y = oldCamPos.y;
-		}
-		
-		cam.transform.position = newPos;
 	}
 
     // FixedUpdate is called once per fixed framerate frame
--- Stranded/Assets/Scripts/RadialMenu.cs
+++ Stranded/Assets/Scripts/RadialMenu.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using GamepadInput;
 
 public class RadialMenu : MonoBehaviour {
 
@@ -9,6 +10,13 @@ public class RadialMenu : MonoBehaviour {
 	bool isActive = false;
 	bool inStageTwo = false;
 	bool showingResult = false;
+	bool usingGamepad = false;
+	bool releasedAfterGamepadSelect = true;
+
+	float RESULT_START_TIME = 1.0f;
+	float resultTimeLeft = -1.0f;
+	GameObject selectedCommandObject = null;
+	GameObject selectedPlayerObject = null;
 
 	float radius = 3f;
 
@@ -23,29 +31,56 @@ public class RadialMenu : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (!releasedAfterGamepadSelect && Input.GetAxis ("TriggersR_0") < 0.5) {
+			releasedAfterGamepadSelect = true;
+		}
+
 		// Middle mouse button
-		if (Input.GetMouseButtonDown (2)) {
+		if (Input.GetMouseButtonDown (2) && !(usingGamepad && isActive)) {
 			isActive = !isActive;
 			if (!isActive) {
-				inStageTwo = false;
-				showingResult = false;
-				foreach (GameObject item in playerMenuItems) {
-					item.renderer.enabled = false;
-				}
-				foreach (GameObject item in commandMenuItems) {
-					item.renderer.enabled = false;
-				}
+				Reset();
+			} else {
+				usingGamepad = false;
 			}
 		}
 
+		if (!showingResult && releasedAfterGamepadSelect) {
+			if (Input.GetAxis ("TriggersR_0") < 0.5 && isActive && usingGamepad) {
+				Reset ();
+				usingGamepad = false;
+			} else if (Input.GetAxis ("TriggersR_0") >= 0.5 && !(isActive && !usingGamepad)) {
+				isActive = true;
+				usingGamepad = true;
+			}
+		}
+
+
 		if (isActive) {
 			if (showingResult) {
-				// blabla
+				SpriteRenderer spriteRenderer = selectedCommandObject.GetComponent<SpriteRenderer>();
+				Color newColor = spriteRenderer.color;
+				resultTimeLeft -= Time.deltaTime;
+				newColor.a = resultTimeLeft;
+
+				selectedCommandObject.transform.position = new Vector2(Camera.main.transform.position.x, Camera.main.transform.position.y);
+
+				if (resultTimeLeft < 0) {
+					newColor.a = 1.0f;
+					selectedCommandObject.renderer.enabled = false;
+					Reset();
+				}
+
+				spriteRenderer.color = newColor;
+
 			} else if (!inStageTwo) {
+				float angleSize = 360.0f / (float)playerMenuItems.Length;
+
 				for (int i = 0; i < playerMenuItems.Length; i++) {
 					GameObject item = playerMenuItems[i];
 
-					float angleDifference = (360.0f / (float)playerMenuItems.Length) * i;
+					float angleDifference = angleSize * i;
 					Vector3 vect = Quaternion.AngleAxis(angleDifference, Vector3.forward) * Vector3.up;
 					vect.Normalize();
 					vect *= radius;
@@ -58,20 +93,22 @@ public class RadialMenu : MonoBehaviour {
 					SpriteRenderer spriteRenderer = item.GetComponent<SpriteRenderer>();
 					Color color = spriteRenderer.color;
 					color.a = 0.75f;
-					if (IsMouseOverObject(item)) {
+					if ((IsMouseOverObject(item) && !usingGamepad) || (IsControllerPointingAtObject(angleDifference, angleSize)) && usingGamepad) {
 						color.a = 1;
 					}
 					spriteRenderer.color = color;
 				}
 
-				if (Input.GetMouseButtonUp(0)) {
+				if ((Input.GetMouseButtonUp(0) && !usingGamepad) || (GamePad.GetButtonDown (GamePad.Button.A, GamePad.Index.One) && usingGamepad)) {
 					foreach (GameObject menuItem in playerMenuItems) {
-						if (IsMouseOverObject(menuItem)) {
+						SpriteRenderer spriteRenderer = menuItem.GetComponent<SpriteRenderer>();
+						if (IsMouseOverObject(menuItem) || (GamePad.GetButtonDown (GamePad.Button.A, GamePad.Index.One) && spriteRenderer.color.a > 0.999)) {
 							menuItem.renderer.enabled = true;
 							Vector3 newPos = Camera.main.transform.position;
 							newPos.z = menuItem.transform.position.z;
 							menuItem.transform.position = newPos;
 							inStageTwo = true;
+							selectedPlayerObject = menuItem;
 						} else {
 							menuItem.renderer.enabled = false;
 						}
@@ -86,7 +123,8 @@ public class RadialMenu : MonoBehaviour {
 				for (int i = 0; i < commandMenuItems.Length; i++) {
 					GameObject item = commandMenuItems[i];
 
-					float angleDifference = (360.0f / (float)commandMenuItems.Length) * i;
+					float angleSize = 360.0f / (float)commandMenuItems.Length;
+					float angleDifference = angleSize * i;
 					Vector3 vect = Quaternion.AngleAxis(angleDifference, Vector3.forward) * Vector3.up;
 					vect.Normalize();
 					vect *= radius;
@@ -99,26 +137,43 @@ public class RadialMenu : MonoBehaviour {
 					SpriteRenderer spriteRenderer = item.GetComponent<SpriteRenderer>();
 					Color color = spriteRenderer.color;
 					color.a = 0.75f;
-					if (IsMouseOverObject(item)) {
+					if ((IsMouseOverObject(item) && !usingGamepad) || (IsControllerPointingAtObject(angleDifference, angleSize)) && usingGamepad) {
 						color.a = 1;
 					}
 					spriteRenderer.color = color;
+
+					selectedPlayerObject.transform.position = new Vector2(Camera.main.transform.position.x, Camera.main.transform.position.y);
 				}
 
-				if (Input.GetMouseButtonUp(0)) {
+				if ((Input.GetMouseButtonUp(0) && !usingGamepad) || (GamePad.GetButtonDown (GamePad.Button.A, GamePad.Index.One) && usingGamepad)) {
 					foreach (GameObject menuItem in commandMenuItems) {
-						if (IsMouseOverObject(menuItem)) {
+						SpriteRenderer spriteRenderer = menuItem.GetComponent<SpriteRenderer>();
+						if (IsMouseOverObject(menuItem) || (GamePad.GetButtonDown (GamePad.Button.A, GamePad.Index.One) && spriteRenderer.color.a > 0.999)) {
 							menuItem.renderer.enabled = true;
 							Vector3 newPos = Camera.main.transform.position;
 							newPos.z = menuItem.transform.position.z;
 							menuItem.transform.position = newPos;
 							showingResult = true;
+							selectedCommandObject = menuItem;
+							resultTimeLeft = RESULT_START_TIME;
+
+							foreach (GameObject obj in playerMenuItems) {
+								obj.renderer.enabled = false;
+							}
+							foreach (GameObject obj in commandMenuItems) {
+								obj.renderer.enabled = false;
+							}
+							selectedCommandObject.renderer.enabled = true;	
+
+							if (usingGamepad) {
+								releasedAfterGamepadSelect = false;
+							}
 						} else {
 							menuItem.renderer.enabled = false;
 						}
 					}
 					if (!showingResult) {
-						foreach (GameObject menuItem in playerMenuItems) {
+						foreach (GameObject menuItem in commandMenuItems) {
 							menuItem.renderer.enabled = true;
 						}
 					}
@@ -137,5 +192,37 @@ public class RadialMenu : MonoBehaviour {
 	bool IsMouseOverObject (GameObject obj) {
 		Vector2 mousePosInWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 		return Vector2.Distance (mousePosInWorld, new Vector2 (obj.transform.position.x, obj.transform.position.y)) < BUTTON_RADIUS;
+	}
+
+	bool IsControllerPointingAtObject (float desiredAngle, float angleDiff) {
+
+		// Negated to make them match Unity's coordinate system
+		float controllerX = -Input.GetAxis ("L_XAxis_0");
+		float controllerY = -Input.GetAxis ("L_YAxis_0");
+
+		if (Mathf.Abs (controllerX) < 0.1 && Mathf.Abs (controllerY) < 0.1) {
+			return false;
+		}
+
+		Vector2 direction = new Vector2(controllerX, controllerY);
+		float angle = Vector2.Angle (Vector2.up, direction);
+		if (controllerX < 0) {
+			angle = 360 - angle;
+		}
+		return Mathf.Abs(Mathf.DeltaAngle (angle, desiredAngle)) < (angleDiff / 2);
+	}
+
+	void Reset () {
+		isActive = false;
+		inStageTwo = false;
+		showingResult = false;
+		foreach (GameObject item in playerMenuItems) {
+			item.renderer.enabled = false;
+		}
+		foreach (GameObject item in commandMenuItems) {
+			item.renderer.enabled = false;
+		}
+		selectedCommandObject = null;
+		selectedPlayerObject = null;
 	}
 }

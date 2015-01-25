5d3f64b055f15ec2a4601e391e4902e2b9fa1886
Andrew <delerat@gmail.com>
2015-01-24 17:53:58 -0800

    Beginning of daynight cycle, personalities, and controller input

---
+++ Stranded/Assets/Scripts/TimeCycle.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeCycle : MonoBehaviour
+{
+
+    int dayLength;
+    int dayCount = 0;
+    int rescueDay = 5;
+    int morningLength = 240;
+    int nightLength = 60;
+    bool dayNight = false; // false = DAY | true = NIGHT.
+
+    float currentTime = 0;
+
+	// Use this for initialization
+	void Start () {
+        dayLength = morningLength + nightLength;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    currentTime += 1 * Time.deltaTime;
+
+
+        if (currentTime > morningLength)
+        {
+            dayNight = true;
+        }
+        else
+        {
+            dayNight = false;
+        }
+
+
+
+        if (Input.GetKey(KeyCode.Space))
+            Debug.Log("Current Time: " + currentTime + "  State: " + dayNight);
+	}
+
+	// FixedUpdate is called once per fixed framerate frame
+	void FixedUpdate () {
+	
+	}
+}
